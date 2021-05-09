095863578663b98d9762aa22f7e7bf21c104f607
Anton <grutoha@gmail.com>
2021-05-09 13:49:10 +0300

    asdasd

--- js/main.js
+++ js/main.js
@@ -1,6 +1,39 @@
 $(function () {
-	$(".select__arrow").click(function () {
-		$(".select__list").toggleClass("active");
-		$(this).toggleClass("active");
+	let menuArrow = document.querySelector(".select__arrow");
+	let selectList = document.querySelector(".select__list");
+	let selectTop = document.querySelector(".select__top");
+	selectTop.addEventListener("click", function () {
+		selectList.classList.toggle("active");
+		menuArrow.classList.toggle("active");
 	});
+
+	let selectedLang = document.querySelector(".select__language");
+	let currentSelectedLang = selectedLang;
+	let langList = document.querySelectorAll(".select__item");
+	let currentId;
+
+	if (langList.length > 0) {
+		for (let i = 0; i < langList.length; i++) {
+			langList[i].id = i;
+			console.log(langList[i]);
+			if (langList[i].id == 0) {
+				currentSelectedLang.innerText = langList[i].querySelector(
+					"p"
+				).innerHTML;
+				langList[i].style.display = "none";
+				currentId = i;
+				console.log(currentId);
+			}
+			langList[i].addEventListener("click", function () {
+				selectList.classList.toggle("active");
+				menuArrow.classList.toggle("active");
+				currentSelectedLang.innerText = langList[i].querySelector(
+					"p"
+				).innerHTML;
+				langList[i].style.display = "none";
+				langList[currentId].style.display = "block";
+				currentId = i;
+			});
+		}
+	}
 });
