1d1988ca5a01dbba0f7a5690e0c467aca444d5c9
Anton <grutoha@gmail.com>
2021-05-09 18:34:10 +0300

    asdasdasd

--- js/main.js
+++ js/main.js
@@ -12,28 +12,50 @@ $(function () {
 	let langList = document.querySelectorAll(".select__item");
 	let currentId;
 
+	// if (langList.length > 0) {
+	// 	for (let i = 0; i < langList.length; i++) {
+	// 		langList[i].id = i;
+	// 		if (langList[i].id == 0) {
+	// 			currentSelectedLang.innerText = langList[i].querySelector(
+	// 				"p"
+	// 			).innerHTML;
+	// 			langList[i].style.display = "none";
+	// 			currentId = i;
+	// 			console.log(currentId);
+	// 		}
+
+	// 		langList[i].addEventListener("click", function () {
+	// 			selectList.classList.toggle("active");
+	// 			menuArrow.classList.toggle("active");
+	// 			currentSelectedLang.innerText = langList[i].querySelector(
+	// 				"p"
+	// 			).innerHTML;
+	// 			langList[i].style.display = "none";
+	// 			langList[currentId].style.display = "block";
+	// 			currentId = i;
+	// 		});
+	// 	}
+	// }
+
 	if (langList.length > 0) {
-		for (let i = 0; i < langList.length; i++) {
-			langList[i].id = i;
-			if (langList[i].id == 0) {
-				currentSelectedLang.innerText = langList[i].querySelector(
-					"p"
-				).innerHTML;
-				langList[i].style.display = "none";
-				currentId = i;
-				console.log(currentId);
+		langList.forEach((langItem, index) => {
+			langItem.id = index;
+			if (langItem.id == 0) {
+				currentSelectedLang.innerText = langItem.querySelector("p").innerHTML;
+				langItem.style.display = "none";
+				currentId = index;
 			}
 
-			langList[i].addEventListener("click", function () {
+			langItem.addEventListener("click", function () {
 				selectList.classList.toggle("active");
 				menuArrow.classList.toggle("active");
-				currentSelectedLang.innerText = langList[i].querySelector(
+				currentSelectedLang.innerText = langItem.querySelector(
 					"p"
 				).innerHTML;
-				langList[i].style.display = "none";
-				langList[currentId].style.display = "block";
-				currentId = i;
+				langItem.style.display = "none";
+				langItem[currentId].style.display = "block";
+				currentId = index;
 			});
-		}
+		});
 	}
 });
